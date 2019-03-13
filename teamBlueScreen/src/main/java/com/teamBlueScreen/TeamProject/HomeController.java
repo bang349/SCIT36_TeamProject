bcda820f20107f4033df3b3e9022fa8f167bc843
bangKaram <bang349@naver.com>
2019-03-13 14:33:20 +0900

    커밋 테스트입니다2

--- teamBlueScreen/src/main/java/com/teamBlueScreen/TeamProject/HomeController.java
+++ teamBlueScreen/src/main/java/com/teamBlueScreen/TeamProject/HomeController.java
@@ -33,6 +33,8 @@ public class HomeController {
 		
 		model.addAttribute("serverTime", formattedDate );
 		System.out.println("test2");
+		int a = 100;
+		System.out.println(a+10);
 		return "home";
 	}
 	
